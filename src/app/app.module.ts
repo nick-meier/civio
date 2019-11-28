7c80c1875af83added9a2a2c99a7781123fbbd9c
Nick Meier <nickmeier@berkeley.edu>
2019-11-28 11:07:15 -0800

    initial files for real though

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {
+        path: "game",
+        component: GameComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { PaperScope, Project, Path, Color, Point } from 'paper';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements AfterViewInit {
+  @ViewChild("paperCanvas") canvasElement: ElementRef;
+
+  private scope: PaperScope;
+  private project: Project;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.scope = new PaperScope();
+    this.project = new Project(this.canvasElement.nativeElement);
+
+    for (let i = 0; i < 64; i++) {
+      for (let j = 0; j < 64; j++) {
+        const hexagon = this.createHexagon(50);
+        hexagon.position.x = 50 * i;
+        hexagon.position.y = 50 * i;
+        this.project.activeLayer.addChild(hexagon);
+      }
+    }
+  }
+
+  createHexagon(radius: number): Path {
+    const hexagon = new Path();
+    hexagon.strokeColor = new Color(0, 0, 0);
+    const angle = ((2 * Math.PI) / 6);
+    for (let i = 0; i <= 6; i++) {
+      hexagon.add(new Point(radius * Math.cos(angle * i), radius * Math.sin(angle * i)))
+    }
+    return hexagon;
+  }
+
+}
