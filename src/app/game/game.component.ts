eb9d743386c34a43a6caad86fa52c9d8ecebc053
Nick Meier <nick@eaiti.com>
2019-11-30 16:09:08 -0800

    a lot of updates - textures for hexes, updates to city icon, randomization improvements

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { PaperScope, Project, Path, Color, Point, Item } from 'paper';
+import { PaperScope, Project, Path, Color, Point, Item, Group, Raster, Size } from 'paper';
 
 class Player {
   color: Color;
@@ -9,7 +9,8 @@ class Tile {
   x: number;
   y: number;
   neighbors: Tile[];
-  path: Path;
+  group: Group;
+  // path: Path;
 
   hasBuilding: boolean;
 }
@@ -35,8 +36,8 @@ export class GameComponent implements AfterViewInit {
     this.scope = new PaperScope();
     this.project = new Project(this.canvasElement.nativeElement);
 
-    const rows = 10;
-    const columns = 8;
+    const rows = 18;
+    const columns = 23;
     this.createTiles(rows, columns);
     this.createHexagonGrid(this.tiles);
 
@@ -62,13 +63,30 @@ export class GameComponent implements AfterViewInit {
 
   }
 
-  getEmptyTile(): Tile {
-    return this.allTiles.find((tile: Tile) => {
-      if (tile.hasBuilding) return false;
-      if (tile.neighbors.find((tile: Tile) => tile.hasBuilding)) return false;
-      return true;
-    });
+  *range(start: number, end: number): IterableIterator<number> {
+    for (let i = start; i <= end; i++) {
+      yield i;
+    }
   }
+
+  getEmptyTile(): Tile {
+    const indexList = [...this.range(0, this.allTiles.length - 1)];
+    this.shuffleArray(indexList);
+    for (let i = 0; i < indexList.length; i++) {
+      const tile = this.allTiles[indexList[i]];
+      if (tile.hasBuilding) continue;
+      if (tile.neighbors.find((tile) => tile.hasBuilding)) continue;
+      return tile;
+    }
+    return null;
+
+    // return this.allTiles.find((tile: Tile) => {
+    //   if (tile.hasBuilding) return false;
+    //   if (tile.neighbors.find((tile: Tile) => tile.hasBuilding)) return false;
+    //   return true;
+    // });
+  }
+
 
   createTiles(rows: number, columns: number) {
     this.tiles = [];
@@ -121,7 +139,7 @@ export class GameComponent implements AfterViewInit {
   }
 
   // create grid give then tile data map????
-  createHexagonGrid(tiles: Object[][]) {
+  createHexagonGrid(tiles: Tile[][]) {
     const columns = tiles[0].length;
     const rows = tiles.length;
     const hexSize = 50;
@@ -129,19 +147,52 @@ export class GameComponent implements AfterViewInit {
     const yOffset = 3 / 4 * hexHeight;
     const hexWidth = Math.sqrt(3) * hexSize;
     const xOffset = hexWidth;
+    const forestImages = [
+      'assets/images/unlicensed/m11-247-forest.png',
+      'assets/images/unlicensed/ddg-44-forest.png'
+    ]
+    const mapGroup = new Group();
     for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
-        const hexagon = this.createHexagon(hexSize);
-        this.tiles[j][i].path = hexagon;
-        hexagon.position.x = xOffset * i;
-        if (j % 2 == 0) {
-          hexagon.position.x += xOffset / 2;
-        }
-        hexagon.position.y = yOffset * j;
+        const forestImagePath = forestImages[Math.floor(Math.random() * forestImages.length)];
+        const hexagon = this.createTexturedHexagon(forestImagePath, hexSize);
+        // const hexagon = this.createHexagon(hexSize);
+        this.tiles[j][i].group = hexagon;
+        const offset = new Point(
+          xOffset * i + ((j % 2 == 0) ? xOffset / 2 : 0),
+          yOffset * j
+        )
+        hexagon.translate(offset);
+        // hexagon.position.x = xOffset * i;
+        // if (j % 2 == 0) {
+        //   hexagon.position.x += xOffset / 2;
+        // }
+        // hexagon.position.y = yOffset * j;
         // hexagon.fillColor = new Color(Math.random(), Math.random(), Math.random())
-        this.project.activeLayer.addChild(hexagon);
+        mapGroup.addChild(hexagon);
       }
     }
+    mapGroup.translate(new Point(25 * Math.sqrt(3), 50));
+    this.project.activeLayer.addChild(mapGroup);
+  }
+
+  createTexturedHexagon(imgPath: string, hexSize: number): Group {
+    const clippingGroup = new Group();
+    clippingGroup.clipped = true;
+    const hexagon = this.createHexagon(hexSize);
+    hexagon.clipMask = true;
+    clippingGroup.addChild(hexagon);
+    const raster = new Raster(imgPath);
+    clippingGroup.addChild(raster);
+
+    const pathBounds = hexagon.bounds;
+    const rasterBounds = raster.bounds;
+    const widthRatio = rasterBounds.x / pathBounds.x;
+    const heightRatio = rasterBounds.y / pathBounds.y;
+    const scaleFactor = 1 / Math.min(widthRatio, heightRatio);
+    raster.scale(scaleFactor);
+
+    return clippingGroup;
   }
 
   createHexagon(radius: number): Path {
@@ -156,12 +207,33 @@ export class GameComponent implements AfterViewInit {
 
   createCity(player: Player, tile: Tile) {
     tile.hasBuilding = true;
-    this.project.importSVG('assets/svg/city-svgrepo-com.svg', (item: Item) => {
+    // (tile.group.children[1] as Raster).visible = false;
+    this.project.importSVG('assets/svg/city-svgrepo-com-filled.svg', (item: Item) => {
       item.scale(0.1);
-      item.position = tile.path.position;
-      tile.path.fillColor = player.color;
+      item.position = tile.group.position;
+      const tilePath = tile.group.children[0];
+      tilePath.fillColor = player.color;
+
+      // item.fillColor = new Color(1, 1, 1);
+      // item.strokeColor = new Color(0, 0, 0);
+      // item.strokeWidth = .2;
     });
 
+  }
+
+  // Fisher-Yates shuffle
+  shuffleArray<T>(array: Array<T>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.round(Math.random() * i);
+      const item = array[i];
+      array[i] = array[j];
+      array[j] = item;
+    }
+  }
+
+  canvasScroll(event) {
+    event.preventDefault();
+    console.log('test', event);
   }
 
   // resizeCanvas() {
